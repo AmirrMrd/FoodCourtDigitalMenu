bed5582bcf150013cf74766ff77d4d5a3d5d027e
hosein boobaee <m.hossein.boobaee@hotmail.com>
2023-12-08 16:41:43 +0330

    1402/09/17

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using FoodCourtDigitalMenu;
+using Microsoft.AspNetCore;
+using SepidzWebLocal;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateWebHostBuilder(args).Build().Run();
+
+    }
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+    {
+        return WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>();
+
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,195 @@
+﻿
+using FoodCourtDigitalMenu.Repository;
+using FoodCourtDigitalMenu.Services;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.FileProviders;
+namespace SepidzWebLocal
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            IConfigurationBuilder builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        [System.Obsolete]
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IBranchRepository, BranchRepository>();
+            services.AddScoped<IBranchCrudService, BranchCrudService>();
+            services.AddScoped<IDataService, DataService>();
+
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
+            // Add For Get Connection String From appsetting.json
+            //services.AddDbContext<SepidzDb>(options =>
+            //     options.UseSqlServer(Configuration.GetConnectionString("SepidzDb")));
+            // Set BranchCode
+         
+            services.AddSignalR(hubOptions =>
+            {
+                hubOptions.EnableDetailedErrors = true;
+                //hubOptions.KeepAliveInterval = TimeSpan.FromDays(60);
+            });
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+            /// compression Data Transfer GZip
+            services.Configure<GzipCompressionProviderOptions>(options => options.Level = System.IO.Compression.CompressionLevel.Fastest);
+            services.AddResponseCompression(options =>
+            {
+                //options.E
+                //nableForHttps = true;
+                options.Providers.Add<BrotliCompressionProvider>();
+                options.Providers.Add<GzipCompressionProvider>();
+                options.MimeTypes = new[]
+                {
+            // Default
+            "text/plain",
+            "text/css",
+            "text/scss",
+            "application/javascript",
+            "text/html",
+            "application/xml",
+            "text/xml",
+            "application/json",
+            "text/json",
+            // Custom
+            "image/svg+xml",
+            "image/png",
+            "image/tiff",
+            "image/jpeg"
+        };
+            });
+            #region Athentication
+            //Athentication
+            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            //.AddJwtBearer(options =>
+            //{
+            //    options.TokenValidationParameters = new TokenValidationParameters
+            //    {
+            //        ValidateIssuer = true,
+            //        ValidateAudience = true,
+            //        ValidateLifetime = true,
+            //        ValidateIssuerSigningKey = true,
+            //        ValidIssuer = "Fiver.Security.Bearer",
+            //        ValidAudience = "Fiver.Security.Bearer",
+            //        IssuerSigningKey = JwtSecurityKey.Create("fiver-secret-key")
+            //    };
+
+            //    options.Events = new JwtBearerEvents
+            //    {
+            //        OnAuthenticationFailed = context =>
+            //        {
+            //            return Task.CompletedTask;
+            //        },
+            //        OnTokenValidated = context =>
+            //        {
+            //            return Task.CompletedTask;
+            //        }
+            //    };
+            //});
+            #endregion
+
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Member",
+                    policy => policy.RequireClaim("MembershipId"));
+            });
+
+            //services.AddTransient<IPrintService, PrintService>();
+            //services.AddTransient<IDocumentService, DocumentService>();
+            //services.AddTransient<IJwtTokenBuilder, JwtTokenBuilder>();
+
+            services.AddSignalR(hubOptions =>
+            {
+                hubOptions.EnableDetailedErrors = true;
+                //hubOptions.KeepAliveInterval = TimeSpan.FromDays(60);
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowMyOrigin",
+                builder => builder.WithOrigins("*"));
+            });
+
+            services.AddControllersWithViews();
+            // In production, the Angular files will be served from this directory
+           
+          
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            var contentRoot = env.ContentRootPath;
+            var licenseFile = System.IO.Path.Combine(contentRoot, "Reports", "license.key");
+            //Stimulsoft.Base.StiLicense.LoadFromFile(licenseFile);
+
+            /// Enable Gzip
+            app.UseResponseCompression();
+            if (env.IsDevelopment() || env.IsProduction())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            if (!env.IsDevelopment())
+            {
+                app.UseStaticFiles();
+            }
+
+            app.UseRouting();
+            app.UseCors("MyPolicy");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+            //app.UseSignalR(routes =>
+            //{
+            //    routes.MapHub<TableHub>("/TableHub");
+            //});
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                //spa.Options.SourcePath = "ClientApp";
+
+                //if (env.IsDevelopment())
+                //{
+                //    spa.UseAngularCliServer(npmScript: "start");
+                //}
+                spa.Options.SourcePath = "ClientApp";
+                if (env.IsDevelopment())
+                {
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+                }
+            });
+        }
+    }
+}
